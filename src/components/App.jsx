2208fb6945e7f880c27af209341d5ddaa93a1e0e
Oduor Omondi <soo2117@columbia.edu>
2023-12-15 17:04:24 -0500

    Working on Final project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -7,19 +7,46 @@ import CreateArea from "./CreateArea";
 function App() {
   const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
+  useEffect(() => {
+    fetch('mongodb+srv://omondistanley82:<Stanley2002.>@cluster0.utiqesn.mongodb.net/?retryWrites=true&w=majority')
+      .then(response => response.json())
+      .then(data => setNotes(data));
+  }, []);
+
+  /*function addNote(newNote) {
     setNotes((prevNotes) => {
       return [...prevNotes, newNote];
     });
+  }*/
+  function addNote(newNote) {
+    fetch('mongodb+srv://omondistanley82:<Stanley2002.>@cluster0.utiqesn.mongodb.net/?retryWrites=true&w=majority', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newNote),
+    })
+    .then(response => response.json())
+    .then(data => {
+      setNotes(prevNotes => [...prevNotes, data]);
+    });
   }
 
+
   function deleteNote(id) {
+    fetch(`mongodb+srv://omondistanley82:<Stanley2002.>@cluster0.utiqesn.mongodb.net/?retryWrites=true&w=majority${id}`, {
+      method: 'DELETE'
+    }).then(() => {
+      setNotes(prevNotes => prevNotes.filter((noteItem, index) => index !== id));
+    });
+  }
+  /*function deleteNote(id) {
     setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
     });
-  }
+  }*/
 
   return (
     <div>
