6aeb4b0c5647e5d7b7957ec1af93965e9a007552
Oduor S <omondistanley82@gmail.com>
2023-12-16 01:51:16 +0000

    feat: Update server.js and index.js
    Updated server.js with new MongoDB string and routes. Added instructions for React app in index.js. Added tasks.json and devcontainer.json files.

--- server.js
+++ server.js
@@ -1,81 +1,82 @@
 import Express from "express";
 import mongoose from "mongoose";
 
-const note   = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
+const note = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
 });
 
-const mongoString = 'mongodb+srv://omondistanley82:<Stanley2002.>@cluster0.utiqesn.mongodb.net/?retryWrites=true&w=majority'
+const mongoString =
+  "mongodb+srv://dimmed82:dimmed82@cluster0.bquhibd.mongodb.net/?retryWrites=true&w=majority";
 const app = Express();
 
 mongoose.connect(mongoString);
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal server error');
-    next();
+  console.error(err);
+  res.status(500).send("Internal server error");
+  next();
 });
 
-app.post('/notes', async (req, res) => {
-    const { title, content } = req.body;
-  
-    try {
-      const newNote = new Note({ title, content });
-      await newNote.save();
-      res.json(newNote);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Server error');
-    }
-  });
+app.post("/notes", async (req, res) => {
+  const { title, content } = req.body;
 
-app.get('/notes', async(req, res) => {
-    try {
-        const notes = await Note.find({});
-        res.json(notes);
-    }catch(error) {
-        console.error(error);
-        res.status(500).send('Error retrieving notes');
-    }
+  try {
+    const newNote = new Note({ title, content });
+    await newNote.save();
+    res.json(newNote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
 });
 
-app.put('/notes/:id', async (req, res) => {
-    const { title, content } = req.body; 
-    const noteId = req.params.id; 
-  
-    try {
-      const updatedNote = await Note.findByIdAndUpdate(noteId, { title, content }, { new: true }); 
-      res.json(updatedNote); 
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error updating note');
-    }
-  });
+app.get("/notes", async (req, res) => {
+  try {
+    const notes = await Note.find({});
+    res.json(notes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving notes");
+  }
+});
 
-  app.delete('/notes/:id', async (req, res) => {
-    const noteId = req.params.id; // Get note ID from URL parameter
-  
-    try {
-      await Note.findByIdAndDelete(noteId); 
-      res.json({ message: 'Note deleted successfully' }); 
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error deleting note');
-    }
-  });
-  
-  // Start the server
-  app.listen(3001, () => {
-    console.log('Server started on port 3001');
-  });
-  
+app.put("/notes/:id", async (req, res) => {
+  const { title, content } = req.body;
+  const noteId = req.params.id;
 
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(
+      noteId,
+      { title, content },
+      { new: true },
+    );
+    res.json(updatedNote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error updating note");
+  }
+});
 
+app.delete("/notes/:id", async (req, res) => {
+  const noteId = req.params.id; // Get note ID from URL parameter
 
+  try {
+    await Note.findByIdAndDelete(noteId);
+    res.json({ message: "Note deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error deleting note");
+  }
+});
+
+// Start the server
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
+});
